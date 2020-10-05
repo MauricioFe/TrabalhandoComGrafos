cce57c4fc097a96141c919536b8b331194663132
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-05 16:22:27 -0300

    Implementando um grafo simpels

--- Grafos/Grafos/Program.cs
+++ Grafos/Grafos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,8 +12,35 @@ namespace Grafos
     {
         static void Main(string[] args)
         {
-            
+
+            int numVertices = 0, numArestas = 0;
+            Console.WriteLine("Numero de vertices");
+            numVertices = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero de arestas");
+            numArestas = int.Parse(Console.ReadLine());
+            int[,] grafo = new int[numVertices, numArestas];
+            //string[] nomeVertices = new ;
+            for (int i = 0; i < numVertices; i++)
+            {
+                for (int j = 0; j < numArestas; j++)
+                {
+                    Console.WriteLine($"Confirme as ligações entre o vértice {i} e o vértice {j} com bits");
+                    grafo[i, j] = int.Parse(Console.ReadLine());
+                }
+              
+            }
+            Console.WriteLine("O grafo representado em matrizes adjacentes é: ");
+            for (int i = 0; i < numVertices; i++)
+            {
+                for (int j = 0; j < numArestas; j++)
+                {
+                    Console.Write(grafo[i, j] + "\t");  
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
+
         }
+
     }
 }
